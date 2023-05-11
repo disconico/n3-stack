eef8dea8fbafb75bb0332d190e097df5cf6084d2
Nicolas Guerin <disconico@outlook.com>
2023-05-11 14:44:06 +0200

    Add app configuration

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,16 +1,42 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/db.config';
+import { UsersModule } from './users/users.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'client', 'dist'),
     }),
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        transform: true, // this will ensure we receive the objects on its correct prototype and not a plain object
+        transformOptions: {
+          enableImplicitConversion: true, // this will allow us to remove @Transform decorator on primitive types and Serializable types
+        },
+        whitelist: true,
+        stopAtFirstError: true,
+      }),
+    },
+  ],
 })
 export class AppModule {}
