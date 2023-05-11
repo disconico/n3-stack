0d6e8f605b8db26810d70b7cdabd564a1f9b0d87
Nicolas Guerin <disconico@outlook.com>
2023-05-11 14:50:38 +0200

    Add client fprm to add user

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,15 +2,35 @@ import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { User } from 'shared';
 
 function App() {
 	const [greeting, setGreeting] = useState('');
+	const [userName, setUsername] = useState('');
 
 	useEffect(() => {
 		fetch('/server')
 			.then((res) => res.text())
 			.then((data) => setGreeting(data));
 	}, []);
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		fetch('/server/users', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ username: userName }),
+		})
+			.then((res) => res.json())
+			.then((data: User) => {
+				console.log(data);
+				setUsername('');
+				setGreeting(data.username);
+			});
+	};
 
 	return (
 		<>
@@ -23,6 +43,19 @@ function App() {
 				</a>
 			</div>
 			<h1>{greeting}</h1>
+			<div>
+				<form onSubmit={handleSubmit}>
+					<label htmlFor='username'>Username: </label>
+					<input
+						type='text'
+						id='username'
+						name='username'
+						value={userName}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<button type='submit'>submit</button>
+				</form>
+			</div>
 		</>
 	);
 }
